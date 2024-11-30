7d2dbd30ca76437da721df2dc77efcfce195e930
Adela Ramadhina <choysanthemum@gmail.com>
2024-12-01 01:43:55 +1100

    added roblox gif hehheheeh

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,15 @@
 export default function NotFound() {
   return (
-    <section>
+    <section className="text-center">
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         404 - Page Not Found
       </h1>
-      <p className="mb-4">Oops! I don't exist.</p>
+      <p className="mb-4">Oops! I don't exist. Here's my Roblox character dancing though.</p>
+      <img
+        src="/image/404.gif"
+        alt="404 Not Found"
+        className="mx-auto rounded-lg shadow-lg"
+      />
     </section>
-  )
+  );
 }
